1da3827c368fcf6e5b1abc89785996809b6f37ba
Manasi Ramkrushna Patil <145982061+manasi2654@users.noreply.github.com>
2026-04-13 10:26:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,16 @@
 print("Welcome To Docker")
+from flask import Flask
+from PIL import Image
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    # Create a simple image
+    img = Image.new('RGB', (200, 200), color='blue')
+    img.save("output.png")
+
+    return "Image created successfully!"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
